21e49b5dbd538c988e17579a8c358f9014a6e1e4
Bruce Johnston <bruce.johnston@microsoft.com>
2016-12-08 16:19:13 -0800

    Adding alternate UploadDocuments example
    We're adding a second, more complex example of uploading documents. This is
    the code embedded in the .NET "Getting Started" article here:
    
    https://docs.microsoft.com/azure/search/search-import-data-dotnet

--- DotNetHowTo/DotNetHowTo/Program.cs
+++ DotNetHowTo/DotNetHowTo/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#define HowToExample
+
+using System;
 using System.Configuration;
 using System.Linq;
 using System.Threading;
@@ -71,6 +73,8 @@ namespace AzureSearch.SDKHowTo
             serviceClient.Indexes.Create(definition);
         }
 
+#if HowToExample
+
         private static void UploadDocuments(ISearchIndexClient indexClient)
         {
             var hotels = new Hotel[]
@@ -132,6 +136,76 @@ namespace AzureSearch.SDKHowTo
             Console.WriteLine("Waiting for documents to be indexed...\n");
             Thread.Sleep(2000);
         }
+
+#else
+
+        private static void UploadDocuments(ISearchIndexClient indexClient)
+        {
+            var actions =
+                new IndexAction<Hotel>[]
+                {
+                    IndexAction.Upload(
+                        new Hotel()
+                        {
+                            HotelId = "1",
+                            BaseRate = 199.0,
+                            Description = "Best hotel in town",
+                            DescriptionFr = "Meilleur hôtel en ville",
+                            HotelName = "Fancy Stay",
+                            Category = "Luxury",
+                            Tags = new[] { "pool", "view", "wifi", "concierge" },
+                            ParkingIncluded = false,
+                            SmokingAllowed = false,
+                            LastRenovationDate = new DateTimeOffset(2010, 6, 27, 0, 0, 0, TimeSpan.Zero),
+                            Rating = 5,
+                            Location = GeographyPoint.Create(47.678581, -122.131577)
+                        }),
+                    IndexAction.Upload(
+                        new Hotel()
+                        {
+                            HotelId = "2",
+                            BaseRate = 79.99,
+                            Description = "Cheapest hotel in town",
+                            DescriptionFr = "Hôtel le moins cher en ville",
+                            HotelName = "Roach Motel",
+                            Category = "Budget",
+                            Tags = new[] { "motel", "budget" },
+                            ParkingIncluded = true,
+                            SmokingAllowed = true,
+                            LastRenovationDate = new DateTimeOffset(1982, 4, 28, 0, 0, 0, TimeSpan.Zero),
+                            Rating = 1,
+                            Location = GeographyPoint.Create(49.678581, -122.131577)
+                        }),
+                    IndexAction.MergeOrUpload(
+                        new Hotel()
+                        {
+                            HotelId = "3",
+                            BaseRate = 129.99,
+                            Description = "Close to town hall and the river"
+                        }),
+                    IndexAction.Delete(new Hotel() { HotelId = "6" })
+                };
+
+            var batch = IndexBatch.New(actions);
+
+            try
+            {
+                indexClient.Documents.Index(batch);
+            }
+            catch (IndexBatchException e)
+            {
+                // Sometimes when your Search service is under load, indexing will fail for some of the documents in
+                // the batch. Depending on your application, you can take compensating actions like delaying and
+                // retrying. For this simple demo, we just log the failed document keys and continue.
+                Console.WriteLine(
+                    "Failed to index some of the documents: {0}",
+                    String.Join(", ", e.IndexingResults.Where(r => !r.Succeeded).Select(r => r.Key)));
+            }
+
+            Console.WriteLine("Waiting for documents to be indexed...\n");
+            Thread.Sleep(2000);
+        }
+#endif
 
         private static void RunQueries(ISearchIndexClient indexClient)
         {
