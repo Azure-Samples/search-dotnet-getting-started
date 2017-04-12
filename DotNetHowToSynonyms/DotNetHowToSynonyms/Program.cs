8b0c5fd09b8958c44383fd326737d761e1df5b46
Nate Ko <nateko@microsoft.com>
2017-04-11 17:35:06 -0700

    Update ProjectGuid. Cleanup the sample code.

--- DotNetHowToSynonyms/DotNetHowToSynonyms/Program.cs
+++ DotNetHowToSynonyms/DotNetHowToSynonyms/Program.cs
@@ -12,13 +12,13 @@ namespace AzureSearch.SDKHowToSynonyms
 {
     class Program
     {
-        // This sample shows how to delete, create, upload documents and query an index
+        // This sample shows how to delete, create, upload documents and query an index with a synonym map
         static void Main(string[] args)
         {
             SearchServiceClient serviceClient = CreateSearchServiceClient();
 
-            Console.WriteLine("{0}", "Deleting index...\n");
-            DeleteHotelsIndexIfExists(serviceClient);
+            Console.WriteLine("{0}", "Cleaning up resources...\n");
+            CleanupResources(serviceClient);
 
             Console.WriteLine("{0}", "Creating index...\n");
             CreateHotelsIndex(serviceClient);
@@ -29,8 +29,6 @@ namespace AzureSearch.SDKHowToSynonyms
             UploadDocuments(indexClient);
 
             ISearchIndexClient indexClientForQueries = CreateSearchIndexClient();
-
-            RunQueries(indexClientForQueries);
 
             RunQueriesWithNonExistentTermsInIndex(indexClientForQueries);
 
@@ -49,7 +47,7 @@ namespace AzureSearch.SDKHowToSynonyms
         private static SearchServiceClient CreateSearchServiceClient()
         {
             string searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
-            string adminApiKey = ConfigurationManager.AppSettings["SearchServiceAdminApiKey"];
+            string adminApiKey = ConfigurationManager.AppSettings["SearchServiceApiKey"];
 
             SearchServiceClient serviceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(adminApiKey));
             return serviceClient;
@@ -58,13 +56,13 @@ namespace AzureSearch.SDKHowToSynonyms
         private static SearchIndexClient CreateSearchIndexClient()
         {
             string searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
-            string queryApiKey = ConfigurationManager.AppSettings["SearchServiceQueryApiKey"];
+            string queryApiKey = ConfigurationManager.AppSettings["SearchServiceApiKey"];
 
             SearchIndexClient indexClient = new SearchIndexClient(searchServiceName, "hotels", new SearchCredentials(queryApiKey));
             return indexClient;
         }
 
-        private static void DeleteHotelsIndexIfExists(SearchServiceClient serviceClient)
+        private static void CleanupResources(SearchServiceClient serviceClient)
         {
             if (serviceClient.Indexes.Exists("hotels"))
             {
@@ -96,8 +94,6 @@ namespace AzureSearch.SDKHowToSynonyms
 
             serviceClient.Indexes.CreateOrUpdate(index);
         }
-
-#if HowToExample
 
         private static void UploadSynonyms(SearchServiceClient serviceClient)
         {
@@ -173,131 +169,6 @@ namespace AzureSearch.SDKHowToSynonyms
             Thread.Sleep(2000);
         }
 
-#else
-
-        private static void UploadDocuments(ISearchIndexClient indexClient)
-        {
-            var actions =
-                new IndexAction<Hotel>[]
-                {
-                    IndexAction.Upload(
-                        new Hotel()
-                        {
-                            HotelId = "1",
-                            BaseRate = 199.0,
-                            Description = "Best hotel in town",
-                            DescriptionFr = "Meilleur hôtel en ville",
-                            HotelName = "Fancy Stay",
-                            Category = "Luxury",
-                            Tags = new[] { "pool", "view", "wifi", "concierge" },
-                            ParkingIncluded = false,
-                            SmokingAllowed = false,
-                            LastRenovationDate = new DateTimeOffset(2010, 6, 27, 0, 0, 0, TimeSpan.Zero),
-                            Rating = 5,
-                            Location = GeographyPoint.Create(47.678581, -122.131577)
-                        }),
-                    IndexAction.Upload(
-                        new Hotel()
-                        {
-                            HotelId = "2",
-                            BaseRate = 79.99,
-                            Description = "Cheapest hotel in town",
-                            DescriptionFr = "Hôtel le moins cher en ville",
-                            HotelName = "Roach Motel",
-                            Category = "Budget",
-                            Tags = new[] { "motel", "budget" },
-                            ParkingIncluded = true,
-                            SmokingAllowed = true,
-                            LastRenovationDate = new DateTimeOffset(1982, 4, 28, 0, 0, 0, TimeSpan.Zero),
-                            Rating = 1,
-                            Location = GeographyPoint.Create(49.678581, -122.131577)
-                        }),
-                    IndexAction.MergeOrUpload(
-                        new Hotel()
-                        {
-                            HotelId = "3",
-                            BaseRate = 129.99,
-                            Description = "Close to town hall and the river"
-                        }),
-                    IndexAction.Delete(new Hotel() { HotelId = "6" })
-                };
-
-            var batch = IndexBatch.New(actions);
-
-            try
-            {
-                indexClient.Documents.Index(batch);
-            }
-            catch (IndexBatchException e)
-            {
-                // Sometimes when your Search service is under load, indexing will fail for some of the documents in
-                // the batch. Depending on your application, you can take compensating actions like delaying and
-                // retrying. For this simple demo, we just log the failed document keys and continue.
-                Console.WriteLine(
-                    "Failed to index some of the documents: {0}",
-                    String.Join(", ", e.IndexingResults.Where(r => !r.Succeeded).Select(r => r.Key)));
-            }
-
-            Console.WriteLine("Waiting for documents to be indexed...\n");
-            Thread.Sleep(2000);
-        }
-#endif
-
-        private static void RunQueries(ISearchIndexClient indexClient)
-        {
-            SearchParameters parameters;
-            DocumentSearchResult<Hotel> results;
-
-            Console.WriteLine("Search the entire index for the term 'budget' and return only the hotelName field:\n");
-
-            parameters =
-                new SearchParameters()
-                {
-                    Select = new[] { "hotelName" }
-                };
-
-            results = indexClient.Documents.Search<Hotel>("budget", parameters);
-
-            WriteDocuments(results);
-
-            Console.Write("Apply a filter to the index to find hotels cheaper than $150 per night, ");
-            Console.WriteLine("and return the hotelId and description:\n");
-
-            parameters =
-                new SearchParameters()
-                {
-                    Filter = "baseRate lt 150",
-                    Select = new[] { "hotelId", "description" }
-                };
-
-            results = indexClient.Documents.Search<Hotel>("*", parameters);
-
-            WriteDocuments(results);
-
-            Console.Write("Search the entire index, order by a specific field (lastRenovationDate) ");
-            Console.Write("in descending order, take the top two results, and show only hotelName and ");
-            Console.WriteLine("lastRenovationDate:\n");
-
-            parameters =
-                new SearchParameters()
-                {
-                    OrderBy = new[] { "lastRenovationDate desc" },
-                    Select = new[] { "hotelName", "lastRenovationDate" },
-                    Top = 2
-                };
-
-            results = indexClient.Documents.Search<Hotel>("*", parameters);
-
-            WriteDocuments(results);
-
-            Console.WriteLine("Search the entire index for the term 'motel':\n");
-
-            parameters = new SearchParameters();
-            results = indexClient.Documents.Search<Hotel>("motel", parameters);
-
-            WriteDocuments(results);
-        }
-
         private static void RunQueriesWithNonExistentTermsInIndex(ISearchIndexClient indexClient)
         {
             SearchParameters parameters;
@@ -316,12 +187,12 @@ namespace AzureSearch.SDKHowToSynonyms
             results = indexClient.Documents.Search<Hotel>("\"five star\"", parameters);
             WriteDocuments(results);
 
-            Console.WriteLine("Search the entire index for the terms 'economy' and 'hotel':\n");
-            results = indexClient.Documents.Search<Hotel>("economy hotel", parameters);
-            WriteDocuments(results);
-
             Console.WriteLine("Search the entire index for the term 'internet':\n");
             results = indexClient.Documents.Search<Hotel>("internet", parameters);
+            WriteDocuments(results);
+
+            Console.WriteLine("Search the entire index for the terms 'economy' AND 'hotel':\n");
+            results = indexClient.Documents.Search<Hotel>("economy hotel", parameters);
             WriteDocuments(results);
         }
 
