300cab28e8f896f5ad75bf84f7c509f8b2c3db40
Matt Gotteiner <magottei@microsoft.com>
2017-10-11 15:59:01 -0700

    initial commit

---
+++ DotNetHowToIndexers/Program.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Spatial;
+
+namespace AzureSearch.SDKHowTo
+{
+    /// <summary>
+    /// Demo of Azure Search indexers for Azure SQL, Azure Table Storage, and Azure Cosmos DB.
+    /// </summary>
+    sealed class Program
+    {
+        private const string ConfigFilePath = "appsettings.json";
+
+        private const string AzureSqlOption = "AzureSQL";
+        private const string AzureTableStorageOption = "AzureTableStorage";
+        private const string AzureCosmosDbOption = "AzureCosmosDB";
+
+        private const string IndexName = "hotels";
+
+        private const string AzureSqlConnectionStringProperty = "AzureSqlConnectionString";
+        private const string AzureStorageConnectionStringProperty = "AzureStorageConnectionString";
+        private const string AzureStorageTableNameProperty = "AzureStorageTableName";
+        private const string AzureCosmosDbConnectionStringProperty = "AzureCosmosDbConnectionString";
+        private const string AzureCosmosDbCollectionNameProperty = "AzureCosmosDbCollectionName";
+
+        private const string AzureSqlTableName = "hotels";
+        private const string AzureSqlHighWaterMarkColumnName = "RowVersion";
+        private const string AzureSqlDataSourceName = "azure-sql";
+        private const string AzureSqlIndexerName = "azure-sql-indexer";
+
+        private const string AzureTableStorageDataSourceName = "azure-table-storage";
+        private const string AzureTableStorageIndexerName = "azure-table-storage-indexer";
+
+        private const string AzureCosmosDbDataSourceName = "azure-cosmos-db";
+        private const string AzureCosmosDbIndexerName = "azure-cosmos-db-indexer";
+
+        private const string AzureSoftDeleteColumnName = "IsDeleted";
+        private const string AzureSoftDeleteMarkerValue = "true";
+
+        /// <summary>
+        /// Specifies a soft column delete policy to be used with Azure Search data sources.
+        /// </summary>
+        /// <remarks>
+        /// If the soft delete column has the marker value, the entire row or record is considered to be
+        /// deleted
+        /// See https://docs.microsoft.com/en-us/rest/api/searchservice/create-data-source for more information.
+        /// </remarks>
+        private static DataDeletionDetectionPolicy SoftDeleteColumnPolicy = new SoftDeleteColumnDeletionDetectionPolicy
+        {
+            SoftDeleteColumnName = AzureSoftDeleteColumnName,
+            SoftDeleteMarkerValue = AzureSoftDeleteMarkerValue
+        };
+
+        /// <summary>
+        /// Specifies a schedule for how often an Azure Search indexer runs.
+        /// </summary>
+        /// <remarks>
+        /// See https://docs.microsoft.com/en-us/rest/api/searchservice/create-indexer for more information.
+        /// </remarks>
+        private static IndexingSchedule IndexerSchedule = new IndexingSchedule
+        {
+            Interval = TimeSpan.FromMinutes(30),
+            StartTime = DateTimeOffset.Now
+        };
+
+        private static void Usage()
+        {
+            Console.Error.WriteLine($"Usage: DotNetHowToIndexers ({AzureSqlOption}|{AzureTableStorageOption}|{AzureCosmosDbOption})");
+            Console.Error.WriteLine(AzureSqlOption);
+            Console.Error.WriteLine($"\tSpecify the {AzureSqlConnectionStringProperty} property in {ConfigFilePath}");
+            Console.Error.WriteLine(AzureTableStorageOption);
+            Console.Error.WriteLine($"\tSpecify the {AzureStorageConnectionStringProperty} and {AzureStorageTableNameProperty} properties in {ConfigFilePath}");
+            Console.Error.WriteLine(AzureCosmosDbOption);
+            Console.Error.WriteLine($"\tSpecify the {AzureCosmosDbConnectionStringProperty} and {AzureCosmosDbCollectionNameProperty} properties in {ConfigFilePath}");
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Usage();
+                return;
+            }
+
+            IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile(ConfigFilePath);
+            IConfigurationRoot configuration = builder.Build();
+
+            SearchServiceClient serviceClient = CreateSearchServiceClient(configuration);
+
+            string option = args[0];
+            string indexerName;
+            if (option.Equals(AzureSqlOption, StringComparison.OrdinalIgnoreCase))
+            {
+                indexerName = SetupAzureSqlIndexer(serviceClient, configuration);
+            }
+            else if (option.Equals(AzureTableStorageOption, StringComparison.OrdinalIgnoreCase))
+            {
+                indexerName = SetupAzureTableStorageIndexer(serviceClient, configuration);
+            }
+            else if (option.Equals(AzureCosmosDbOption, StringComparison.OrdinalIgnoreCase))
+            {
+                indexerName = SetupAzureCosmosDbIndexer(serviceClient, configuration);
+            }
+            else
+            {
+                Usage();
+                return;
+            }
+
+            Console.WriteLine("Deleting index...");
+            DeleteIndexIfExists(serviceClient);
+
+            Console.WriteLine("Creating index...");
+            CreateIndex(serviceClient);
+
+            Console.WriteLine("Running indexer...");
+            serviceClient.Indexers.Run(indexerName);
+
+            Console.WriteLine("Done...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Creates an indexer which pulls data from an Azure SQL database and puts it
+        /// inside the hotels index.
+        /// </summary>
+        private static string SetupAzureSqlIndexer(SearchServiceClient serviceClient, IConfigurationRoot configuration)
+        {
+            Console.WriteLine("Deleting Azure SQL data source if it exists...");
+            DeleteDataSourceIfExists(serviceClient, AzureSqlDataSourceName);
+
+            Console.WriteLine("Creating Azure SQL data source...");
+            DataSource azureSqlDataSource = CreateAzureSqlDataSource(serviceClient, configuration);
+
+            Console.WriteLine("Deleting Azure SQL indexer if it exists...");
+            DeleteIndexerIfExists(serviceClient, AzureSqlIndexerName);
+
+            Console.WriteLine("Creating Azure SQL indexer...");
+            Indexer azureSqlIndexer = CreateIndexer(serviceClient, AzureSqlDataSourceName, AzureSqlIndexerName);
+
+            return azureSqlIndexer.Name;
+        }
+
+        /// <summary>
+        /// Creates an indexer which pulls data from Azure Table Storage and puts it
+        /// inside the hotels index.
+        /// </summary>
+        private static string SetupAzureTableStorageIndexer(SearchServiceClient serviceClient, IConfigurationRoot configuration)
+        {
+            Console.WriteLine("Deleting Azure Table Storage data source if it exists...");
+            DeleteDataSourceIfExists(serviceClient, AzureTableStorageDataSourceName);
+
+            Console.WriteLine("Creating Azure Table Storage data source...");
+            DataSource azureTableStorageDataSource = CreateAzureTableStorageDataSource(serviceClient, configuration);
+
+            Console.WriteLine("Deleting Azure Table Storage indexer...");
+            DeleteIndexerIfExists(serviceClient, AzureTableStorageIndexerName);
+
+            Console.WriteLine("Creating Azure Table Storage indexer...");
+            Indexer azureTableStorageIndexer = CreateIndexer(serviceClient, AzureTableStorageDataSourceName, AzureTableStorageIndexerName);
+
+            return azureTableStorageIndexer.Name;
+        }
+
+        /// <summary>
+        /// Creates an indexer which pulls data from Azure Cosmos DB and puts it
+        /// inside the hotels index.
+        /// </summary>
+        private static string SetupAzureCosmosDbIndexer(SearchServiceClient serviceClient, IConfigurationRoot configuration)
+        {
+            Console.WriteLine("Deleting Azure Cosmos DB data source...");
+            DeleteDataSourceIfExists(serviceClient, AzureCosmosDbDataSourceName);
+
+            Console.WriteLine("Creating Cosmos DB data source...");
+            DataSource azureCosmosDbDataSource = CreateAzureCosmosDbDataSource(serviceClient, configuration);
+
+            Console.WriteLine("Deleting Cosmos DB indexer...");
+            DeleteIndexerIfExists(serviceClient, AzureCosmosDbIndexerName);
+
+            Console.WriteLine("Creating Cosmos DB indexer...");
+            Indexer azureCosmosDbIndexer = CreateIndexer(serviceClient, AzureCosmosDbDataSourceName, AzureCosmosDbIndexerName);
+
+            return azureCosmosDbIndexer.Name;
+        }
+
+        /// <summary>
+        /// Creates the SearchServiceClient used to make API requests against
+        /// your Azure Search service.
+        /// </summary>
+        private static SearchServiceClient CreateSearchServiceClient(IConfigurationRoot configuration)
+        {
+            string searchServiceName = configuration["SearchServiceName"];
+            string adminApiKey = configuration["SearchServiceAdminApiKey"];
+
+            SearchServiceClient serviceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(adminApiKey));
+            return serviceClient;
+        }
+
+        /// <summary>
+        /// Delete an Azure Search index for the Hotel data type, if it exists.
+        /// </summary>
+        private static void DeleteIndexIfExists(SearchServiceClient serviceClient)
+        {
+            if (serviceClient.Indexes.Exists(IndexName))
+            {
+                serviceClient.Indexes.Delete(IndexName);
+            }
+        }
+
+        /// <summary>
+        /// Creates an Azure Search index for the Hotel data type.
+        /// </summary>
+        private static void CreateIndex(SearchServiceClient serviceClient)
+        {
+            var definition = new Index()
+            {
+                Name = IndexName,
+                Fields = FieldBuilder.BuildForType<Hotel>()
+            };
+
+            serviceClient.Indexes.Create(definition);
+        }
+
+        /// <summary>
+        /// Deletes an Azure Search data source with the given name, if it exists.
+        /// </summary>
+        private static void DeleteDataSourceIfExists(SearchServiceClient serviceClient, string dataSourceName)
+        {
+            if (serviceClient.DataSources.Exists(dataSourceName))
+            {
+                serviceClient.DataSources.Delete(dataSourceName);
+            }
+        }
+
+        /// <summary>
+        /// Creates an Azure Search indexer for the given data source with the given name.
+        /// </summary>
+        private static Indexer CreateIndexer(SearchServiceClient serviceClient, string dataSourceName, string indexerName)
+        {
+            Indexer indexer = new Indexer
+            {
+                DataSourceName = dataSourceName,
+                Name = indexerName,
+                Schedule = IndexerSchedule,
+                TargetIndexName = IndexName
+            };
+            return serviceClient.Indexers.Create(indexer);
+        }
+
+        /// <summary>
+        /// Deletes an Azure Search indexer if it exists.
+        /// </summary>
+        private static void DeleteIndexerIfExists(SearchServiceClient serviceClient, string indexerName)
+        {
+            if (serviceClient.Indexers.Exists(indexerName))
+            {
+                serviceClient.Indexers.Delete(indexerName);
+            }
+        }
+
+        /// <summary>
+        /// Creates an Azure Search data source for a Azure SQL database.
+        /// </summary>
+        /// <remarks>
+        /// Requires a connection string be specified in the configuration file.
+        /// The table name is assumed to be "hotels" as specified in the accompanying
+        /// sql script.
+        /// </remarks>
+        private static DataSource CreateAzureSqlDataSource(SearchServiceClient serviceClient, IConfigurationRoot configuration)
+        {
+            DataSource azureSqlDataSource = DataSource.AzureSql(
+                name: AzureSqlDataSourceName,
+                sqlConnectionString: configuration[AzureSqlConnectionStringProperty],
+                tableOrViewName: AzureSqlTableName,
+                changeDetectionPolicy: new HighWaterMarkChangeDetectionPolicy
+                {
+                    HighWaterMarkColumnName = AzureSqlHighWaterMarkColumnName
+                },
+                deletionDetectionPolicy: SoftDeleteColumnPolicy);
+            return serviceClient.DataSources.Create(azureSqlDataSource);
+        }
+
+        /// <summary>
+        /// Creates an Azure Search data source for Azure Table Storage.
+        /// </summary>
+        /// <remarks>
+        /// Requires a connection string be specified in the configuration file.
+        /// Requires a table name be specified in the configuration file.
+        /// </remarks>
+        private static DataSource CreateAzureTableStorageDataSource(SearchServiceClient serviceClient, IConfigurationRoot configuration)
+        {
+            DataSource azureTableStorageDataSource = DataSource.AzureTableStorage(
+                name: AzureTableStorageDataSourceName,
+                storageConnectionString: configuration[AzureStorageConnectionStringProperty],
+                tableName: configuration[AzureStorageTableNameProperty],
+                query: null,
+                deletionDetectionPolicy: SoftDeleteColumnPolicy);
+            return serviceClient.DataSources.Create(azureTableStorageDataSource);
+        }
+
+        /// <summary>
+        /// Creates an Azure Search data source for Azure Cosmos DB.
+        /// </summary>
+        /// <remarks>
+        /// Requires a connection string with a database name be specified in the configuration file.
+        /// Requires an Azure Cosmos DB collection name be specified in the configuration file.
+        /// </remarks>
+        private static DataSource CreateAzureCosmosDbDataSource(SearchServiceClient serviceClient, IConfigurationRoot configuration)
+        {
+            DataSource azureCosmosDbDataSource = DataSource.DocumentDb(
+                name: AzureCosmosDbDataSourceName,
+                documentDbConnectionString: configuration[AzureCosmosDbConnectionStringProperty],
+                collectionName: configuration[AzureCosmosDbCollectionNameProperty],
+                deletionDetectionPolicy: SoftDeleteColumnPolicy);
+            return serviceClient.DataSources.Create(azureCosmosDbDataSource);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -12,6 +12,14 @@ This sample includes three projects that are meant to help when getting started 
 * Upload Documents to an Azure Search index
 * Search & Filter documents within an index
 
+The second sample DotNetHowToIndexers is a simple .NET Core console application that shows how to:
+
+* Create and Delete an Azure Search index
+* Create an Azure Search indexer for the following types of data sources:
+  * Azure SQL
+  * Azure Table Storage
+  * Azure Cosmos DB
+
 The second sample DotNetHowToSynonyms demonstrates how to incorporate the Synonyms feature in your application step by step:
 
 * Create an Azure Search index
@@ -29,6 +37,24 @@ Lastly, the sample AzureSearchDotNetSample is a more detailed example that demon
 * Open the DotNetHowTo.sln project in Visual Studio
 * Update the appsettings.json with the service and api details of your Azure Search service
 * Compile and Run the project
+
+## Running the DotNetHowToIndexers sample
+
+* Open the DotNetHowToIndexers.csproj project in Visual Studio.
+* Update the appsettings.json with the service and api details of your Azure Search service,
+  along with whatever data source you choose (Azure SQL, Azure Table Storage, or Azure Cosmos DB).
+  * If you are choosing to use Azure SQL, run the `data\hotels.sql` script against your Azure SQL database
+    to populate it with suitable sample data.
+  * If you are choosing to use Azure Table Storage run `powershell data\hotels-table-storage.ps1 -StorageAccountName <Your storage account name> -StorageAccountKey <Your storage account key>.
+    This will automatically create a `hotels` table under your storage account with the data set in `data\hotels.json`.
+  * If you are choosing to use Azure Cosmos DB, upload `data\hotels.json` to a Cosmos DB collection of your choice.
+    Follow the instructions at https://docs.microsoft.com/en-us/azure/cosmos-db/import-data#JSON.
+* Compile and Run the project, specifying the correct command line parameter for which data source you are using:
+  * AzureSQL for Azure SQL
+  * AzureTableStorage for Azure Table Storage
+  * AzureCosmosDB for Azure Cosmos DB
+* Alternatively, download the .NET Core SDK at https://www.microsoft.com/net/core and
+  issue a `dotnet run <DataSource>` command from the DotNetHowToIndexers directory.
 
 ## Running the DotNetHowToSynonyms sample
 
