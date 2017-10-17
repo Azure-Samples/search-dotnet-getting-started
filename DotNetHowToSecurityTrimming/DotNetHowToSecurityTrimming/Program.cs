d03d3399230dcf92c6c22ed17914913ef2f38f19
Revital Barletz <revitalb@microsoft.com>
2017-10-17 13:29:11 -0700

    First draft.

--- DotNetHowToSecurityTrimming/DotNetHowToSecurityTrimming/Program.cs
+++ DotNetHowToSecurityTrimming/DotNetHowToSecurityTrimming/Program.cs
@@ -11,6 +11,13 @@ using Microsoft.Azure.Search;
 using Microsoft.Azure.Search.Models;
 using Microsoft.Graph;
 using Microsoft.Identity.Client;
+using System.Net.Http;
+using System.Text;
+using System.Collections.Concurrent;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Dynamic;
+using Newtonsoft.Json.Converters;
 
 namespace DotNetHowToSecurityTrimming
 {
@@ -23,6 +30,11 @@ namespace DotNetHowToSecurityTrimming
 
         private static ISearchServiceClient _searchClient;
         private static ISearchIndexClient _indexClient;
+
+        private static string _token;
+
+        private static ConcurrentDictionary<string, List<string>> _groupsCache = new ConcurrentDictionary<string, List<string>>();
+        private static GraphServiceClient _graph;
 
         static void Main(string[] args)
         {
@@ -47,10 +59,13 @@ namespace DotNetHowToSecurityTrimming
             string indexName = "securedfiles";
 
             // AAD Initialization
-            GraphServiceClient graph = CreateGraphServiceClient();
+            _graph = CreateGraphServiceClient();
 
             // Create a group, a user and associate both
-            List<string> groups = CreateUsersAndGroups(graph).Result;
+            List<string> groups = CreateUsersAndGroups().Result;
+
+            // Create a cache that contains the users and the list of groups they are part of
+            RefreshCache();
 
             // Create an HTTP reference to the catalog index
             _searchClient = new SearchServiceClient(searchServiceName, new SearchCredentials(apiKey));
@@ -70,8 +85,17 @@ namespace DotNetHowToSecurityTrimming
             {
                 // Retrieve user's groups so that a search filter could be built using the groups list
                 Console.WriteLine("Get groups for user {0}...\n", user);
-                var groupsIdForUser = GetGroupIdsForUser(graph, user).Result;
-                SearchQueryWithFilter(groupsIdForUser);
+                RefreshCacheIfRequired(user);
+                SearchQueryWithFilter(user);
+            }
+        }
+
+        private static void RefreshCacheIfRequired(string user)
+        {
+            if (!_groupsCache.ContainsKey(user))
+            {
+                var groups = GetGroupIdsForUser(user).Result;
+                _groupsCache[user] = groups;
             }
         }
 
@@ -107,18 +131,75 @@ namespace DotNetHowToSecurityTrimming
                 {
                     result = app.AcquireTokenAsync(scopes).Result;
                 }
-                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", result.AccessToken);
+                _token = result.AccessToken;
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", _token);
                 return Task.FromResult(0);
             }));
         }
 
-        private static void SearchQueryWithFilter(List<string> groups)
+        private static async void RefreshCache()
+        {
+            HttpClient client = new HttpClient();
+
+            string requestContent = BuildBatchRequest();
+            //_token = "eyJ0eXAiOiJKV1QiLCJub25jZSI6IkFRQUJBQUFBQUFCSGg0a21TX2FLVDVYcmp6eFJBdEh6NHhGR3dzYkNJRlJySHYyT0RzMlZkLXR0b0V1MHp5ME42ZWZzbURUeDZPQl81X3l4ZVRjTDVTWDhwRVRjVkZrQzdtZUJSeGpFa0ZsSzJ3SUpDNzBjYVNBQSIsImFsZyI6IlJTMjU2IiwieDV0IjoiMktWY3V6cUFpZE9McVdTYW9sN3dnRlJHQ1lvIiwia2lkIjoiMktWY3V6cUFpZE9McVdTYW9sN3dnRlJHQ1lvIn0.eyJhdWQiOiJodHRwczovL2dyYXBoLm1pY3Jvc29mdC5jb20iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8xZmQ2NjM3MC1mZTU3LTQ5OWQtYmZkMy1iYWRlOWU4OTZmZTgvIiwiaWF0IjoxNTA4MTk5MDQ1LCJuYmYiOjE1MDgxOTkwNDUsImV4cCI6MTUwODIwMjk0NSwiYWNyIjoiMSIsImFpbyI6IkFTUUEyLzhHQUFBQXFxOEtwN011TXg1T1NNZmh2Ujk2N2ZESDRnWDhLMFl1M016d3I1NitpYUk9IiwiYW1yIjpbInB3ZCJdLCJhcHBfZGlzcGxheW5hbWUiOiJyZXZpdGFsYnRlc3R0b2tlbjIiLCJhcHBpZCI6ImExYjZmZjY2LWQwODctNDJkZC1hZjk5LTdkYWI1YjQzZTU2YiIsImFwcGlkYWNyIjoiMCIsImRldmljZWlkIjoiZDAzN2I5YTQtZTU0Zi00ZTdmLWI4N2ItOTA4YjVkZDhkNzkzIiwiZV9leHAiOjI2MjgwMCwiZmFtaWx5X25hbWUiOiJCYXJsZXR6IiwiZ2l2ZW5fbmFtZSI6IlJldml0YWwiLCJpcGFkZHIiOiIxNjcuMjIwLjAuMTk0IiwibmFtZSI6IlJldml0YWwgQmFybGV0eiIsIm9pZCI6ImY2YWUyOGI5LWRhOWItNDNlOC05YjUyLTE4OGE5N2RmNmEzOSIsInBsYXRmIjoiMyIsInB1aWQiOiIxMDAzM0ZGRkE0MEJBMEFDIiwic2NwIjoiRGlyZWN0b3J5LkFjY2Vzc0FzVXNlci5BbGwgRGlyZWN0b3J5LlJlYWQuQWxsIERpcmVjdG9yeS5SZWFkV3JpdGUuQWxsIEdyb3VwLlJlYWQuQWxsIFVzZXIuUmVhZCIsInNpZ25pbl9zdGF0ZSI6WyJrbXNpIl0sInN1YiI6ImljNnV2Ni1rc3FwOFNzV1VJblExajYtZHNtRzFES1praTVoQlhtcFZKUFEiLCJ0aWQiOiIxZmQ2NjM3MC1mZTU3LTQ5OWQtYmZkMy1iYWRlOWU4OTZmZTgiLCJ1bmlxdWVfbmFtZSI6InJldml0YWxiQHJldml0YWxzLm9ubWljcm9zb2Z0LmNvbSIsInVwbiI6InJldml0YWxiQHJldml0YWxzLm9ubWljcm9zb2Z0LmNvbSIsInV0aSI6InNFZnRWRFVJSlVLdUdzeDc4cGRKQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbIjYyZTkwMzk0LTY5ZjUtNDIzNy05MTkwLTAxMjE3NzE0NWUxMCJdfQ.IErAycrSRgZG-PallPOYJNS_jo_6h_KDuzTEhhbweCq7OYEGEFqrachF3H5ZjiT75LKwyaCm7zXMfqB4L76xt_EykjD7epKegeEIMHvGnWLYt8E7C_Wg2egmV375EFCi6HFaGfVmSSfOufsbJevSAiSPhn7wjFnGnIzGSYD3fnCWC9IVX5__ceec-gf98d-xWXaUWopCMaxaMKwIEUNwSewuVQHE-ayJhwNDU4tUIGw1msnbC5PzpUPkkAeWSP1joV3FqNeiUjYuSdpLMWt9Uw5gGEsx6v1qlwlIVInRBsN792T19Gx35FB9COI6KZ9jo_h2UY0b0eVJbhR2DiAHOQ";
+            string responseString = await GetResponseContent(client, requestContent);
+
+            Result data = JsonConvert.DeserializeObject<Result>(responseString);
+            _groupsCache.Clear();
+            for (int i = 0; i < data.Responses.Count(); i++)
+            {
+                List<string> userGroups = new List<string>();
+                for (int j = 0; j < data.Responses[i].Body.Value.Count(); j++)
+                {
+                    ResponseBodyValue value = data.Responses[i].Body.Value[j];
+                    if (value.Type == "#microsoft.graph.group")
+                    {
+                        userGroups.Add(value.Id);
+                    }
+                }
+                int id = Convert.ToInt32(data.Responses[i].Id);
+                string key = UsersPrincipalNameGroupA[id];
+                _groupsCache[key] = userGroups;
+            }
+        }
+
+        private static async Task<string> GetResponseContent(HttpClient client, string requestContent)
+        {
+            client.DefaultRequestHeaders.Add("Authorization", string.Format("bearer {0}", _token));
+            var stringContent = new StringContent(requestContent, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("https://graph.microsoft.com/beta/$batch", stringContent);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            return responseString;
+        }
+
+        private static string BuildBatchRequest()
+        {
+            string requestBodyFormat = @"{{ ""requests"": [ {0} ]  }}";
+            string requestsBodyFormat = @"{{
+                    ""id"":""{0}"",
+                    ""method"":""GET"",
+                    ""url"":""users/{1}/memberOf""
+                }},";
+
+            string requestsBody = null;
+            //requestsBody += string.Format(requestsBodyFormat, "3", "revitalb@revitals.onmicrosoft.com");
+            for (int i = 0; i < UsersPrincipalNameGroupA.Count; i++)
+            {
+                requestsBody += string.Format(requestsBodyFormat, i, UsersPrincipalNameGroupA[i]);
+            }
+            return string.Format(requestBodyFormat, requestsBody);
+        }
+
+        private static void SearchQueryWithFilter(string user)
         {
             SearchParameters parameters;
             DocumentSearchResult<SecuredFiles> results;
             // Using the filter below, the search result will contain all documents, that their GroupIds field contain any one of the 
             // Ids in the groups list
-            string filter = String.Format("groupIds/any(p:search.in(p, '{0}'))", string.Join(",", groups.Select(g => g.ToString())));
+            string filter = String.Format("groupIds/any(p:search.in(p, '{0}'))", string.Join(",", String.Join(",", _groupsCache[user])));
             parameters =
                 new SearchParameters()
                 {
@@ -139,7 +220,7 @@ namespace DotNetHowToSecurityTrimming
                     IndexAction.Upload(
                         new SecuredFiles()
                         {
-                            FileId = "1",
+                            FileId = "11",
                             Size = 126,
                             Name = "secured_file_a",
                             Type = "txt",
@@ -148,7 +229,7 @@ namespace DotNetHowToSecurityTrimming
                     IndexAction.Upload(
                         new SecuredFiles()
                         {
-                            FileId = "2",
+                            FileId = "12",
                             Size = 1266,
                             Name = "secured_file_b",
                             Type = "txt",
@@ -157,7 +238,7 @@ namespace DotNetHowToSecurityTrimming
                     IndexAction.Upload(
                         new SecuredFiles()
                         {
-                            FileId = "3",
+                            FileId = "13",
                             Size = 140,
                             Name = "secured_file_c",
                             Type = "txt",
@@ -221,7 +302,7 @@ namespace DotNetHowToSecurityTrimming
             }
         }
 
-        private static async Task<List<string>> CreateUsersAndGroups(GraphServiceClient graph)
+        private static async Task<List<string>> CreateUsersAndGroups()
         {
             List<string> groups = new List<string>();
 
@@ -233,7 +314,7 @@ namespace DotNetHowToSecurityTrimming
                 group.MailEnabled = false;
                 group.MailNickname = "group1";
                 // Create AAD group
-                Group newGroup = await graph.Groups.Request().AddAsync(group);
+                Group newGroup = await _graph.Groups.Request().AddAsync(group);
                 groups.Add(newGroup.Id);
 
                 User user = new User();
@@ -245,9 +326,9 @@ namespace DotNetHowToSecurityTrimming
                 user.PasswordProfile = new PasswordProfile() { Password = "Test1Test" };
                 user.AccountEnabled = true;
                 // Create AAD user
-                User newUSer = await graph.Users.Request().AddAsync(user);
+                User newUSer = await _graph.Users.Request().AddAsync(user);
                 // Associate user with group
-                await graph.Groups[newGroup.Id].Members.References.Request().AddAsync(newUSer);
+                await _graph.Groups[newGroup.Id].Members.References.Request().AddAsync(newUSer);
 
                 user = new User();
                 user.GivenName = "Second User";
@@ -258,16 +339,16 @@ namespace DotNetHowToSecurityTrimming
                 user.PasswordProfile = new PasswordProfile() { Password = "Test2Test" };
                 user.AccountEnabled = true;
                 // Create AAD user
-                newUSer = await graph.Users.Request().AddAsync(user);
+                newUSer = await _graph.Users.Request().AddAsync(user);
                 // Associate user with group
-                await graph.Groups[newGroup.Id].Members.References.Request().AddAsync(newUSer);
+                await _graph.Groups[newGroup.Id].Members.References.Request().AddAsync(newUSer);
 
                 group = new Group();
                 group.DisplayName = "My Second Prog Group";
                 group.SecurityEnabled = true;
                 group.MailEnabled = false;
                 group.MailNickname = "group2";
-                newGroup = await graph.Groups.Request().AddAsync(group);
+                newGroup = await _graph.Groups.Request().AddAsync(group);
                 // Create AAD group
                 groups.Add(newGroup.Id);
 
@@ -280,9 +361,9 @@ namespace DotNetHowToSecurityTrimming
                 user.PasswordProfile = new PasswordProfile() { Password = "Test3Test" };
                 user.AccountEnabled = true;
                 // Create AAD user
-                newUSer = await graph.Users.Request().AddAsync(user);
+                newUSer = await _graph.Users.Request().AddAsync(user);
                 // Associate user with group
-                await graph.Groups[newGroup.Id].Members.References.Request().AddAsync(newUSer);
+                await _graph.Groups[newGroup.Id].Members.References.Request().AddAsync(newUSer);
             }
             catch (Exception ex)
             {
@@ -292,12 +373,12 @@ namespace DotNetHowToSecurityTrimming
             return groups;
         }
 
-        private static async Task<List<string>> GetGroupIdsForUser(GraphServiceClient graph, string userPrincipalName)
+        private static async Task<List<string>> GetGroupIdsForUser(string userPrincipalName)
         {
             List<string> groups = new List<string>();
             try
             {
-                var allUserGroupsRequest = graph.Users[userPrincipalName].MemberOf.Request();
+                var allUserGroupsRequest = _graph.Users["revitalb@revitals.onmicrosoft.com"].MemberOf.Request();
 
                 while (allUserGroupsRequest != null)
                 {
