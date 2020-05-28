ebabbc619cb38046b74a44b0df485d1ebf63d8f8
Geralt Wang <v-xinlwa@microsoft.com>
2020-05-28 14:47:03 +0800

    Update project DotNetETagsExplainer Azure Search SDK to Track2

--- DotNetETagsExplainer/DotNetETagsExplainer/Program.cs
+++ DotNetETagsExplainer/DotNetETagsExplainer/Program.cs
@@ -1,124 +1,114 @@
-﻿namespace AzureSearch.SDKHowTo
-{
-    using System;
-    using Microsoft.Azure.Search;
-    using Microsoft.Azure.Search.Models;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Rest.Azure;
+﻿using Azure;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
 
+namespace AzureSearch.SDKHowTo
+{
     class Program
     {
+        private static SearchIndex searchIndex;
+
         // This sample shows how ETags work by performing conditional updates and deletes
         // on an Azure Search index.
-        static void Main(string[] args)
+        static async Task Main()
         {
             IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
             IConfigurationRoot configuration = builder.Build();
 
-            SearchServiceClient serviceClient = CreateSearchServiceClient(configuration);
-
-            Console.WriteLine("Deleting index...\n");
-            DeleteTestIndexIfExists(serviceClient);
+            SearchServiceClient searchServiceClient = GetSearchServiceClient(configuration);
 
             // Every top-level resource in Azure Search has an associated ETag that keeps track of which version
             // of the resource you're working on. When you first create a resource such as an index, its ETag is
             // empty.
-            Index index = DefineTestIndex();
+            searchIndex = DefineSearchIndex();
             Console.WriteLine(
-                $"Test index hasn't been created yet, so its ETag should be blank. ETag: '{index.ETag}'");
+                $"Test searchIndex hasn't been created yet, so its ETag should be blank. ETag: '{searchIndex.ETag}'");
 
             // Once the resource exists in Azure Search, its ETag will be populated. Make sure to use the object
             // returned by the SearchServiceClient! Otherwise, you will still have the old object with the
             // blank ETag.
-            Console.WriteLine("Creating index...\n");
-            index = serviceClient.Indexes.Create(index);
-
-            Console.WriteLine($"Test index created; Its ETag should be populated. ETag: '{index.ETag}'");
+            Console.WriteLine("Creating searchIndex...\n");
+            searchIndex = await searchServiceClient.CreateIndexAsync(searchIndex);
+            Console.WriteLine($"Test searchIndex created; Its ETag should be populated. ETag: '{searchIndex.ETag}'");
 
             // ETags let you do some useful things you couldn't do otherwise. For example, by using an If-Match
-            // condition, we can update an index using CreateOrUpdate and be guaranteed that the update will only
+            // condition, we can update an index using CreateOrUpdateIndexAsync() and be guaranteed that the update will only
             // succeed if the index already exists.
-            index.Fields.Add(new Field("name", AnalyzerName.EnMicrosoft));
-            index =
-                serviceClient.Indexes.CreateOrUpdate(
-                    index,
-                    accessCondition: AccessCondition.GenerateIfExistsCondition());
+            SearchField searchField = new SearchField("name", SearchFieldDataType.String) { Analyzer = LexicalAnalyzerName.EnMicrosoft };
+            searchIndex.Fields.Add(searchField);
 
+            searchIndex = await searchServiceClient.CreateOrUpdateIndexAsync(searchIndex);
             Console.WriteLine(
-                $"Test index updated; Its ETag should have changed since it was created. ETag: '{index.ETag}'");
+                $"Test searchIndex updated; Its ETag should have changed since it was created. ETag: '{searchIndex.ETag}'");
 
             // More importantly, ETags protect you from concurrent updates to the same resource. If another
             // client tries to update the resource, it will fail as long as all clients are using the right
             // access conditions.
-            Index indexForClient1 = index;
-            Index indexForClient2 = serviceClient.Indexes.Get("test");
+            SearchIndex indexForClient1 = searchIndex;
+            SearchIndex indexForClient2 = await searchServiceClient.GetIndexAsync("test");
 
             Console.WriteLine("Simulating concurrent update. To start, both clients see the same ETag.");
             Console.WriteLine($"Client 1 ETag: '{indexForClient1.ETag}' Client 2 ETag: '{indexForClient2.ETag}'");
 
             // Client 1 successfully updates the index.
-            indexForClient1.Fields.Add(new Field("a", DataType.Int32));
-            indexForClient1 =
-                serviceClient.Indexes.CreateOrUpdate(
-                    indexForClient1,
-                    accessCondition: AccessCondition.IfNotChanged(indexForClient1));
-
-            Console.WriteLine($"Test index updated by client 1; ETag: '{indexForClient1.ETag}'");
+            indexForClient1.Fields.Add(new SearchField("a", SearchFieldDataType.Int32));
+            indexForClient1 = await searchServiceClient.CreateOrUpdateIndexAsync(indexForClient1);
+            Console.WriteLine($"Test searchIndex updated by client 1; ETag: '{indexForClient1.ETag}'");
 
             // Client 2 tries to update the index, but fails, thanks to the ETag check.
             try
             {
-                indexForClient2.Fields.Add(new Field("b", DataType.Boolean));
-                serviceClient.Indexes.CreateOrUpdate(
-                    indexForClient2, 
-                    accessCondition: AccessCondition.IfNotChanged(indexForClient2));
+                indexForClient2.Fields.Add(new SearchField("b", SearchFieldDataType.Boolean));
+                searchIndex = await searchServiceClient.CreateOrUpdateIndexAsync(indexForClient2);
 
                 Console.WriteLine("Whoops; This shouldn't happen");
                 Environment.Exit(1);
             }
-            catch (CloudException e) when (e.IsAccessConditionFailed())
+            catch (RequestFailedException e) when (e.Status == 400)
             {
-                Console.WriteLine("Client 2 failed to update the index, as expected.");
+                Console.WriteLine("Client 2 failed to update the searchIndex, as expected.");
             }
+            finally
+            {
+                // You can also use access conditions with Delete operations. For example, you can implement an
+                // atomic version of the DeleteTestIndexIfExists method from this sample like this:
+                Console.WriteLine("Deleting searchIndex...\n");
+                await searchServiceClient.DeleteIndexAsync("test");
 
-            // You can also use access conditions with Delete operations. For example, you can implement an
-            // atomic version of the DeleteTestIndexIfExists method from this sample like this:
-            Console.WriteLine("Deleting index...\n");
-            serviceClient.Indexes.Delete("test", accessCondition: AccessCondition.GenerateIfExistsCondition());
+                // This is slightly better than using the Exists method since it makes only one round trip to
+                // Azure Search instead of potentially two. It also avoids an extra Delete request in cases where
+                // the resource is deleted concurrently, but this doesn't matter much since resource deletion in
+                // Azure Search is idempotent.
 
-            // This is slightly better than using the Exists method since it makes only one round trip to
-            // Azure Search instead of potentially two. It also avoids an extra Delete request in cases where
-            // the resource is deleted concurrently, but this doesn't matter much since resource deletion in
-            // Azure Search is idempotent.
-
-            // And we're done! Bye!
-            Console.WriteLine("Complete.  Press any key to end application...\n");
-            Console.ReadKey();
+                // And we're done! Bye!
+                Console.WriteLine("Complete.  Press any key to end application...\n");
+                Console.ReadKey();
+            }
         }
 
-        private static SearchServiceClient CreateSearchServiceClient(IConfigurationRoot configuration)
+        /// <summary>
+        /// Configure the Azure Cognitive Search Endpoint and AdminApiKey in appsetting.json to create a SearchServicClient 
+        /// which use to interact with Azure Cogntive Search Service.
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        private static SearchServiceClient GetSearchServiceClient(IConfigurationRoot configuration)
         {
-            string searchServiceName = configuration["SearchServiceName"];
+            string endPoint = configuration["SearchServicEndpoint"];
             string adminApiKey = configuration["SearchServiceAdminApiKey"];
 
-            SearchServiceClient serviceClient =
-                new SearchServiceClient(searchServiceName, new SearchCredentials(adminApiKey));
-            return serviceClient;
+            SearchServiceClient searchServiceClient = new SearchServiceClient(new Uri(endPoint), new AzureKeyCredential(adminApiKey));
+
+            return searchServiceClient;
         }
 
-        private static void DeleteTestIndexIfExists(SearchServiceClient serviceClient)
-        {
-            if (serviceClient.Indexes.Exists("test"))
+        private static SearchIndex DefineSearchIndex() =>
+            new SearchIndex("test")
             {
-                serviceClient.Indexes.Delete("test");
-            }
-        }
-
-        private static Index DefineTestIndex() =>
-            new Index()
-            {
-                Name = "test",
-                Fields = new[] { new Field("id", DataType.String) { IsKey = true } }
+                Fields = { new SimpleField("id", SearchFieldDataType.String) { IsKey = true } }
             };
     }
 }
