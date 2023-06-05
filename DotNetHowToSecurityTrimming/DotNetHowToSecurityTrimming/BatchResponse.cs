300f61a209dc70d039b7c078280feec2748a69f4
HeidiSteen <heidist@microsoft.com>
2023-06-04 18:04:42 -0700

    Restored file (BatchResponse.cs)

---
+++ DotNetHowToSecurityTrimming/DotNetHowToSecurityTrimming/BatchResponse.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace DotNetHowToSecurityTrimming
+{
+    public class BatchResponse
+    {
+        public string Id { get; set; }
+
+        public BatchResponseBody Body { get; set; }
+    }
+
+    public class BatchResponseBody
+    {
+        public string[] Value { get; set; }
+    }
+
+    public class BatchResult
+    {
+        public BatchResponse[] Responses { get; set; }
+    }
+}
